a167d4093be31e53e051d8a0edbbce994588e870
ruslanvs <ruslanvs@yahoo.com>
2018-01-22 20:03:04 -0800

    Time display app complete

--- apps/Time_Display/views.py
+++ apps/Time_Display/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
 def index ( request ):
-    response = "There we are"
-    return HttpResponse( response )
-    
+    # response = "There we are"
+    # return HttpResponse( response )
+    context = {
+        "time": strftime( "%Y - %m - %d %H:%M %p", gmtime() )
+    }
+    return render( request, "Time_Display/index.html", context )
