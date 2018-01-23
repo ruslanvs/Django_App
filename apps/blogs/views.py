7005f1a52eee9fbfb6c363c7e2d5f81d5203839d
ruslanvs <ruslanvs@yahoo.com>
2018-01-22 19:07:19 -0800

    app dependent routing added

--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, HttpResponse, redirect
 def index( request ):
-    response = "Placeholder to later display all the list of blogs"
-    return HttpResponse( response )
+    # response = "Placeholder to later display all the list of blogs"
+    # return HttpResponse( response )
+    return render( request, "blogs/index.html" )
 
 def new( request ):
     response = "Placeholder to later display a NEW form to display a NEW blog"
     return HttpResponse( response )
     
-def create( request ): 
-    return redirect( index )
-
+def create( request ):
+    if request.method == "POST":
+        print "*"*50
+        print request.POST
+        print request.POST['name']
+        print request.POST['desc']
+        request.session['name'] = "test"
+        print "*"*50
+        return redirect( index )
+    else:
+        return redirect( index )
+        
 def show( request, number ):
     response = "Placeholder to later display blog " + number
     return HttpResponse( response )
