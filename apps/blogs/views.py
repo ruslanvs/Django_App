1ce747e36ef24977ec5b0430b1621d364fdb5bb9
ruslanvs <ruslanvs@yahoo.com>
2018-01-22 16:28:13 -0800

    Initial commit

---
+++ apps/blogs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, HttpResponse, redirect
+def index( request ):
+    response = "Placeholder to later display all the list of blogs"
+    return HttpResponse( response )
+
+# Create your views here.
