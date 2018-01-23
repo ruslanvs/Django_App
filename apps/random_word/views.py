a1a3f69d2c2006de184da850ec6b72a116fce6ca
ruslanvs <ruslanvs@yahoo.com>
2018-01-23 10:29:26 -0800

    Random word complete

--- apps/random_word/views.py
+++ apps/random_word/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+from django.utils.crypto import get_random_string
 def index ( request ):
     context = {
-        "something": "we are here"
+        "random_word": get_random_string( length = 14 )
     }
+
     return render( request, "random_word/index.html", context )
